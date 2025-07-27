4d7b23d72610cbfef0467ec2466fedfa175bbaf8
Debanjan Dasgupta <debanjandasgupta007@gmail.com>
2025-07-27 23:25:23 +0530

    Updated for checking

--- controllers/appointment_controller.js
+++ controllers/appointment_controller.js
@@ -20,6 +20,7 @@ const precheckAndCreateOrder = async (req, res) => {
     try {
         const session_user = req.headers.user;
         if (!session_user) throw new Error('Session expired');
+        console.log(session_user);
 
         const { doctor_id, appointment_date, appointment_time } = req.payload;
         if(new Date(appointment_date) < new Date()) throw new Error('Booking for past date is not allowed');
