230ec3e5cc5e5341e0f804da1332922159384b27
Debanjan Dasgupta <debanjandasgupta007@gmail.com>
2025-07-27 23:32:30 +0530

    Updated urp logger

--- controllers/appointment_controller.js
+++ controllers/appointment_controller.js
@@ -21,8 +21,8 @@ const precheckAndCreateOrder = async (req, res) => {
         const session_user = req.headers.user;
         if (!session_user) throw new Error('Session expired');
         console.log(session_user);
-
         const { doctor_id, appointment_date, appointment_time } = req.payload;
+        console.log(req.payload);
         if(new Date(appointment_date) < new Date()) throw new Error('Booking for past date is not allowed');
         const user = await Users.findOne({ where: { id: session_user.user_id } });
         const doctor = await Doctors.findOne({
