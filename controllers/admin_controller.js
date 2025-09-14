fdd57051f58abf8d5c2b5cb0eb12067a9ed0d01c
Debanjan Dasgupta <debanjandasgupta007@gmail.com>
2025-09-14 21:08:48 +0530

    Updated

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -67,72 +67,72 @@ const send_otp_admin = async (req, res) => {
         }).code(200);
 
     }
-},
+}
 
-    constverify_otp_admin = async (req, res) => {
-        try {
-            const { email, otp } = req.payload;
-            const admin = await Admins.findOne({ where: { email: email } });
-            if (!admin) {
-                return res.response({
-                    success: false,
-                    message: 'Admin not found',
-                })
-            }
-            const otpCode = await Otps.findOne({
-                where: {
-                    id: admin.otp_id
-                },
-                raw: true
-            })
-            const otpTime = new Date(otpCode.otp_time);
-            const currentTime = new Date();
-            // Check if OTP is older than 10 minutes        
-            const diffInMinutes = (currentTime - otpTime) / 1000 / 60;
-            if (diffInMinutes > 10) {
-                return res.response({
-                    success: false,
-                    message: 'OTP expired',
-                })
-            }
-            if (otpCode.otp !== otp) {
-                return res.response({
-                    success: false,
-                    message: 'Invalid OTP',
-                })
-            }
-            const access_token = await JWTFunctions.generateToken({ email: email, id: admin.id, role: 'ADMIN' }, '1h');
-            const refresh_token = await JWTFunctions.generateToken({ email: email, id: admin.id, role: 'ADMIN' }, '1d');
-            await Admins.update({
-                access_token: access_token,
-                refresh_token: refresh_token,
-            }, {
-                where: {
-                    email: email
-                }
-            })
-            return res.response({
-                success: true,
-                message: 'OTP verified successfully',
-                data: {
-                    access_token: access_token,
-                    refresh_token: refresh_token,
-                    admin: {
-                        id: admin.id,
-                        name: admin.name,
-                        email: admin.email
-                    }
-                }
-            })
-        } catch (error) {
-            console.log(error);
+const verify_otp_admin = async (req, res) => {
+    try {
+        const { email, otp } = req.payload;
+        const admin = await Admins.findOne({ where: { email: email } });
+        if (!admin) {
             return res.response({
                 success: false,
-                message: error.message,
-            }).code(200);
-
+                message: 'Admin not found',
+            })
         }
+        const otpCode = await Otps.findOne({
+            where: {
+                id: admin.otp_id
+            },
+            raw: true
+        })
+        const otpTime = new Date(otpCode.otp_time);
+        const currentTime = new Date();
+        // Check if OTP is older than 10 minutes        
+        const diffInMinutes = (currentTime - otpTime) / 1000 / 60;
+        if (diffInMinutes > 10) {
+            return res.response({
+                success: false,
+                message: 'OTP expired',
+            })
+        }
+        if (otpCode.otp !== otp) {
+            return res.response({
+                success: false,
+                message: 'Invalid OTP',
+            })
+        }
+        const access_token = await JWTFunctions.generateToken({ email: email, id: admin.id, role: 'ADMIN' }, '1h');
+        const refresh_token = await JWTFunctions.generateToken({ email: email, id: admin.id, role: 'ADMIN' }, '1d');
+        await Admins.update({
+            access_token: access_token,
+            refresh_token: refresh_token,
+        }, {
+            where: {
+                email: email
+            }
+        })
+        return res.response({
+            success: true,
+            message: 'OTP verified successfully',
+            data: {
+                access_token: access_token,
+                refresh_token: refresh_token,
+                admin: {
+                    id: admin.id,
+                    name: admin.name,
+                    email: admin.email
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.response({
+            success: false,
+            message: error.message,
+        }).code(200);
+
     }
+}
 
 const validateSession = async (req, res) => {
     try {
